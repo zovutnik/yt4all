66396839c47ba2de133a81c5052fa5e598da9c0d
Nikita <awwwkeey@gmail.com>
2023-12-17 23:50:55 +0700

    Create login.tsx

---
+++ app/pages/login.tsx
@@ -0,0 +1,14 @@
+import 'tailwindcss/tailwind.css'
+
+export default function Login() {
+  return (
+    <div className="flex items-center justify-center min-h-screen py-2">
+      <div className="p-6 max-w-sm w-full bg-white shadow-md rounded-md">
+        <div className="flex justify-center items-center">
+          <span className="text-gray-700 font-semibold text-2xl">Login</span>
+        </div>
+        <button className="mt-4 w-full block bg-blue-600 hover:bg-blue-500 text-white font-semibold rounded-lg px-4 py-3">Login with Google</button>
+      </div>
+    </div>
+  )
+}
