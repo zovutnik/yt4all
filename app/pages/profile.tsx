4b11b5f82c5dfcd51abbc9072ab90150100d5542
Nikita <awwwkeey@gmail.com>
2023-12-17 23:48:30 +0700

    Create profile.tsx

---
+++ app/pages/profile.tsx
@@ -0,0 +1,15 @@
+import 'tailwindcss/tailwind.css'
+
+export default function Profile() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">
+        Profile Page
+      </h1>
+
+      <p className="mt-3 text-2xl">
+        This is the profile page.
+      </p>
+    </div>
+  )
+}
